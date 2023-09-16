95e41005958d23f55db60f5ba5d656a30c0cd9aa
Jason Washburn <jason.washburn@gmail.com>
2023-09-16 09:32:04 -0500

    feat: :sparkles: adds training requirements, assignments, completions

--- training/admin.py
+++ training/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 
-from .models import Employee
+from .models import (
+    Employee,
+    TrainingAssignment,
+    TrainingCompletion,
+    TrainingRequirement,
+)
 
 admin.site.register(Employee)
+admin.site.register(TrainingRequirement)
+admin.site.register(TrainingAssignment)
+admin.site.register(TrainingCompletion)
--- training/models.py
+++ training/models.py
@@ -7,3 +7,37 @@ class Employee(models.Model):
 
     def __str__(self):
         return self.user.email
+
+
+class TrainingRequirement(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    link = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+
+class TrainingAssignment(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    training_requirement = models.ForeignKey(
+        TrainingRequirement, on_delete=models.CASCADE
+    )
+    assigned_date = models.DateField()
+    due_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.employee} - {self.training_requirement} - {self.due_date}"
+
+
+class TrainingCompletion(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    training_requirement = models.ForeignKey(
+        TrainingRequirement, on_delete=models.CASCADE
+    )
+    completion_date = models.DateField()
+    # TODO: Add the ability to upload a certificate, store in blob storage like s3, then
+    # store the url to the certificate in this model
+
+    def __str__(self):
+        return f"{self.employee} - {self.training_requirement} - {self.completion_date}"
